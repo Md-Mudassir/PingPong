db6ae110d88745d89dc1a731d008540963f65f22
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-18 14:43:28 +0530

    particles

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 const canvas = document.querySelector("#pong");
+
+//canvas size
 canvas.width = innerWidth;
 canvas.height = innerHeight;
---
+++ js/particles.js
@@ -0,0 +1,9 @@
+let canvas = document.querySelector("canvas");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+let c = canvas.getContext("2d");
+
+let colors = ["#272F32", "#9DBDC6", "#FF3D2E", "#DAEAEF"];
+
